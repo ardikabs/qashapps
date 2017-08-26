76750ca14e57c70e2dffe8487d17dbfef641eb75
Muhammad Fatih <Muhammad Fatih>
2017-08-26 22:11:39 +0700

    add validate name api

--- app/src/main/java/gravicodev/qash/Volley/VolleyHelper.java
+++ app/src/main/java/gravicodev/qash/Volley/VolleyHelper.java
@@ -108,6 +108,71 @@ public class VolleyHelper {
         AppController.getInstance().addToRequestQueue(request, tag_json_obj);
     }
 
+    public void validateName(final VolleyCallback callback, String accountNumber,String name) throws JSONException {
+        // Tag used to cancel the request
+        String tag_json_obj = "json_obj_req";
+
+        String url = "http://finhacks.gravicodev.id/index.php";
+
+
+
+        JSONObject requestJSON = new JSONObject();
+        requestJSON.put("AccountNumber",accountNumber);
+        requestJSON.put("Name",name);
+        requestJSON.put("method","validateName");
+
+        final String requestBody = requestJSON.toString();
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //...
+                        Boolean status = null;
+                        try {
+                            status = response.getBoolean("Status");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        if(status == true) {
+                            callback.onSuccess("Benar");
+                        }
+                        Log.d("volley_log",""+status);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onSuccess("Salah");
+                        //...
+                    }
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(10000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+// Adding request to request queue
+        AppController.getInstance().addToRequestQueue(request, tag_json_obj);
+    }
+
     public void getATM(final VolleyCallback callback, String latitude, String longitude) throws JSONException {
         // Tag used to cancel the request
         String tag_json_obj = "json_obj_req";
